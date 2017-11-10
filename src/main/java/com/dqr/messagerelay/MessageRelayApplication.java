b0f6ff3fce31ffb222294ede0cce19edfae0c10b
David Q. Romney <RomneyDQ@ldschurch.org>
2017-11-10 11:20:00 -0700

    Initial commit

---
+++ src/main/java/com/dqr/messagerelay/MessageRelayApplication.java
@@ -0,0 +1,12 @@
+package com.dqr.messagerelay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageRelayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageRelayApplication.class, args);
+	}
+}
