5ee1576fa595208e5859bb7ee572b8e1cc6d8a3a
David Q. Romney <RomneyDQ@ldschurch.org>
2017-11-16 01:05:23 -0700

    Updated Message model to allow the setting of userId.

--- src/main/java/com/dqr/messagerelay/models/Message.java
+++ src/main/java/com/dqr/messagerelay/models/Message.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Date;
 
 @Entity
 @Table(name = "messages")
@@ -16,11 +17,17 @@ public class Message implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User user;
+    @ManyToOne(fetch = FetchType.EAGER) //TODO this is reversed.  We want to think in objects not relationships.
+   	@JoinColumn(name = "user_id", updatable = false, insertable = false)
+   	private User user;
+
+    @Column(name="user_id")//TODO this is reversed.  We want to think in objects not relationships.
+    private Long userId;
 
     @Column(name = "message")
     private String message;
 
+    @Column(name = "creation_time")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date creationTime;
 }
